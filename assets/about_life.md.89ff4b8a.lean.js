8390852c9515c8646c48c640855b51318ea11ac4
huli66 <huli66@users.noreply.github.com>
2023-03-16 09:37:31 +0000

    deploy: c4233c4643af0240e1b77ca060daea8e886eb56d

---
+++ assets/about_life.md.89ff4b8a.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./app.0a4ff61d.js";const f=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"about/life.md","lastUpdated":1678959422000}'),o={name:"about/life.md"};function r(s,c,n,i,p,d){return a(),t("div")}const l=e(o,[["render",r]]);export{f as __pageData,l as default};
